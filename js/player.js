ecd321594b6a4aac168291c631140fd12b784606
RoryO <rory@metricgnome.net>
2010-12-09 11:33:30 -0600

    Fully passing JSLint strict, better exception handling, better event handling (no IE yet)

--- js/player.js
+++ js/player.js
@@ -1,75 +1,87 @@
-var jsPlayerEngine = function(engineElement, params) {
-  if(!engineElement) {
-    var e = new Error();
-    e.name = "ArgumentError";
-    e.message = "Root player element not provided";
+if (!Array.prototype.last) {
+  Array.prototype.last = function () { 
+    return this[this.length - 1]; 
+  };
+}
+
+var jsPlayerUtils = {
+  exception: function (type, m) {
+    var ex = new Error();
+    ex.name = type;
+    //for Firefox
+    ex.value = type;
+    ex.message = m;
+    throw (ex);
   }
+};
+
+var jsPlayerEngine = function (engineElement, params) {
 
   var outObject = {};
   params = params || {};
-  outObject.engineElement = engineElement;
 
-  outObject.status = function() {
-    if (outObject.engineElement.paused) { return "paused"; }
-    else { return "playing"; }
+  if (!engineElement) {
+    jsPlayerUtils.exception("ArgumentError", "Engine element not provided");
+  } else {
+    outObject.engineElement = engineElement;
   }
 
-  outObject.play = function() {
+  outObject.status = function () {
+    return outObject.engineElement.paused;
+  };
+
+  outObject.play = function () {
     outObject.engineElement.play();
-  }
+  };
 
   outObject.pause = function () {
     outObject.engineElement.pause();
-  }
+  };
 
-  outObject.paused = function () {
+  outObject.isPlaying = function () {
     return outObject.engineElement.paused;
-  }
+  };
 
   return outObject;
-}
+};
 
-var jsPlayer = function(sourceURL, params) {
-  if(!sourceURL) {
-    var e = new Error();
-    e.name = "ArgumentError";
-    e.message = "URL of audio not provided";
-    throw(e);
+var jsPlayer = function (sourceURL, params) {
+  var outObject = {},
+      helpers = {};
+
+  if (!sourceURL) {
+    jsPlayerUtils.exception("ArgumentError", "URL of audio not provided");
   }
 
-  var outObject = {};
   params = params || {};
   outObject.source = sourceURL;
   outObject.elementID = params.elementID || "jsPlayer";
-  outObject.mimeType = function() {
+  outObject.controls = {};
+  outObject.mimeType = (function () {
     var audioTypes = { mp3: "audio/mpeg", mpeg: "audio/mpeg", mpeg3: "audio/mpeg",
-                         ogg: "audio/ogg" };
-    var regex = /\.(.+)$/;
-    var match = regex.exec(outObject.source);
-    var retval;
-    if (match && audioTypes[match[1]]) {
-      retval = audioTypes[match[1]];
+                         ogg: "audio/ogg" },
+        retval, match;
+    console.log(outObject.source.split(".").last());
+    match = outObject.source.split(".").last();
+    if (match && audioTypes[match]) {
+      retval = audioTypes[match];
     } else if (params.format && audioTypes[params.format]) {
       retval = audioTypes[params.format];
     } else {
-      var e = new Error();
-      e.name = "ArgumentError";
-      e.message = "Can not find audio type.  Provide a format member in the parameters object";
-      throw(e);
+      jsPlayerUtils.exception("ArgumentError",
+        "Can not find audio type.  Provide a format member in the parameters object");
     }
     return retval;
-  }();
+  }());
 
   // detect audio engine
-  (function() {
-    var node = document.getElementById(outObject.elementID);
+  (function () {
+    var node = document.getElementById(outObject.elementID),
+        el = document.createElement("audio");
     if (!node) {
-      var ex = new Error();
-      ex.name = "RuntimeError";
-      ex.message = "Unable to find player element " + outObject.elementID;
-      throw(ex);
+      jsPlayerUtils.exception("RuntimeError", 
+        "Unable to find player element " + outObject.elementID);
     }
-    var el = document.createElement("audio");
     if (!el.canPlayType || !el.canPlayType(outObject.mimeType)) {
       // build flash elements
       outObject.engineType = "Flash";
@@ -80,31 +92,37 @@ var jsPlayer = function(sourceURL, params) {
       outObject.engineType =  "Native";
       outObject.engine = jsPlayerEngine(el);
     }
-  })();
+  }());
 
   // construct player controls
-  (function() {
-    var node = document.getElementById(outObject.elementID);
-    var e = document.createElement("div");
+  (function () {
+    var node = document.getElementById(outObject.elementID),
+        e = document.createElement("div");
     e.setAttribute("class", "startStop");
-    // hello IE old friend
-    if (e.addEventListener) {
-      e.addEventListener("click", function() {
-        if (outObject.engine.paused) {
-          outObject.engine.play();
-          e.className = e.className.replace(/\splayerStopped\s/, '');
-          e.className = e.className + " playerStarted"
-        } else {
-          outObject.engine.pause();
-          e.className = e.className.replace(/\splayerStarted\s/, '');
-          e.className = e.className + " playerStopped";
-        }
-      }, false);
-    } else {
-
-    }
     node.appendChild(e);
-  })();
+    console.log("adding controls");
+    outObject.controls.startStop = e;
+  }());
 
+  helpers.playPause = function () {
+    console.log(outObject.engine.isPlaying());
+    if (outObject.engine.isPlaying()) {
+      outObject.engine.play();
+      outObject.controls.startStop.className = outObject.controls.startStop.className.replace(/\splayerStopped\s/, '');
+      outObject.controls.startStop.className = outObject.controls.startStop.className + " playerStarted ";
+    } else {
+      outObject.engine.pause();
+      outObject.controls.startStop.className = outObject.controls.startStop.className.replace(/\splayerStarted\s/, '');
+      outObject.controls.startStop.className = outObject.controls.startStop.className  + " playerStopped ";
+    }
+  };
+
+  //event binding should be absolute last thing
+  outObject.controls.startStop.addEventListener("click", 
+    function () { 
+      helpers.playPause(); 
+    },
+    false);
+  //Hello IE.  We meet again.
   return outObject;
 };
